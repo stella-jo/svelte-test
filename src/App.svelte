2ac428ec33b99ba05e8eab097f508cfb6ea311d7
stella <0604seohee@gmail.com>
2021-12-31 21:08:59 +0900

    Update design and add media query

--- src/App.svelte
+++ src/App.svelte
@@ -13,7 +13,6 @@
 		input.style.display = "block";
 		input.focus();
 	}
-
 	function newTodo() {
 		todoList = todoList.concat({task: newText, checked: false});
 		input.value = null;
@@ -42,49 +41,56 @@
 	});
 </script>
 
-<header>
-	<h1>투두리스트</h1>
-</header>
+<div class = "container">
+	<header>
+		<h1>2022-01-01</h1>
+	</header>
 
-<main>
-	{#each todoList as item, index}
-		<Todos {index} bind:check = {todoList[index].checked} {removeTodo}>{item.task}</Todos>
-	{/each}
-	<input enterkeyhint = "done" bind:value = {newText} class = "todoInput" on:blur = {blurFunction} type = "text">
-	<button class = "addButton" on:click = {add}>
-		<svg width="12" height="12" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-		<rect x="10" y="3" width="4" height="18" rx="1" fill="#DC8F8F"/>
-		<rect x="3" y="10" width="18" height="4" rx="1" fill="#DC8F8F"/>
-		</svg>할 일 추가</button>
-</main>
+	<main>
+		{#each todoList as item, index}
+			<Todos {index} bind:check = {todoList[index].checked} {removeTodo}>{item.task}</Todos>
+		{/each}
+		<input enterkeyhint = "done" bind:value = {newText} class = "todoInput" on:blur = {blurFunction} type = "text">
+		<button class = "addButton" on:click = {add}>
+			<svg width="12" height="12" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+			<rect x="10" y="3" width="4" height="18" rx="1" fill="#DC8F8F"/>
+			<rect x="3" y="10" width="18" height="4" rx="1" fill="#DC8F8F"/>
+			</svg>할 일 추가</button>
+	</main>
+</div>
 
 <style>
+	.container {
+		background-color: #ffffff;
+		display: flex;
+		flex-direction: column;
+		gap: 36px;
+		padding: 36px;
+		max-height: 100vw;
+		max-height: 100vh;
+	}
+
 	header {
 		display: flex;
 		justify-content: space-between;
 		align-items: center;
-		padding: 36px;
 	}
 	h1 {
 		font-size: 2rem;
 		color: #404040;
 	}
 	main {
-		margin: 0px 36px;
 		display: flex;
 		flex-direction: column;
 		gap: 16px;
-	}
-	button {
-		border: none;
-		background: none;
-		text-align: left;
+		overflow: scroll;
 	}
 	.addButton {
 		color: #40404080;
 		display: flex;
 		align-items: center;
 		gap: 4px;
+		width: fit-content;
 	}
 
 	.todoInput {
@@ -95,5 +101,18 @@
 		padding: 8px 0px;
 		display: none;
 		font-size: 1rem;
+	}
+
+	@media screen and (min-device-width: 769px) {
+		.container {
+			width: 400px;
+			height: 600px;
+			border-radius: 36px;
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			transform: translate(-50%, -50%);
+			box-shadow: 0 0 8px 4px #40404010;
+		}
 	}
 </style>
--- src/components/Todos.svelte
+++ src/components/Todos.svelte
@@ -1,16 +1,15 @@
 <script>
     export let index, removeTodo;
     export let check = false;
-    setTimeout(() => {
-        console.log(check);
-    }, 10000);
 </script>
 
 <div class = "box">
-    <label>
-        <input type = "checkbox" bind:checked = {check}>
-        <slot></slot>
-    </label>
+    <form>
+        <input id = {index} type = "checkbox" bind:checked = {check}>
+        <label for = {index}>
+            <slot></slot>
+        </label>
+    </form>
     <button on:click = {removeTodo(index)}>
         <svg width="12" height="12" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
         <rect x="3.51472" y="6.34315" width="4" height="20" rx="1" transform="rotate(-45 3.51472 6.34315)" fill="#DC8F8F"/>
@@ -23,11 +22,17 @@
     .box {
         display: flex;
         justify-content: space-between;
+        gap: 8px;
         align-items: center;
     }
-    button {
-        border: none;
-        background: none;
+    form {
+        display: flex;
+        gap: 8px;
+        word-break: break-all;
+    }
+    input:checked ~ label {
+        text-decoration: line-through;
+        opacity: 50%;
     }
 
     /* input {
